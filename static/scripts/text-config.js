543e9ebfe1cf0abd8320cb43fad7a1acc809d58e
Q1351998764 <57926506+Q1351998764@users.noreply.github.com>
2026-07-15 08:08:41 +0000

    chore: update text config

--- static/scripts/text-config.js
+++ static/scripts/text-config.js
@@ -10,6 +10,22 @@ export default {
   ],
   "entries": [
     {
+      "id": "20260715-080823-说话也有恐怖谷效应吗-",
+      "title": "说话也有恐怖谷效应吗？",
+      "category": "default",
+      "sensitive": false,
+      "documents": [
+        {
+          "path": "art/20260715-080823-说话也有恐怖谷效应吗-.md",
+          "title": "说话也有恐怖谷效应吗？",
+          "excerpt": "说话也有恐怖谷效应吗？ https://www.zhihu.com/question/449596775/answer/2060476277065257065",
+          "markdown": "# 说话也有恐怖谷效应吗？\n\nhttps://www.zhihu.com/question/449596775/answer/2060476277065257065\n",
+          "uploadedAt": "2026-07-15T16:08:26+08:00"
+        }
+      ],
+      "uploadedAt": "2026-07-15T16:08:26+08:00"
+    },
+    {
       "id": "blank",
       "title": "Lorem ipsum",
       "category": "default",
@@ -27,6 +43,7 @@ export default {
     }
   ],
   "uploads": {
+    "art/20260715-080823-说话也有恐怖谷效应吗-.md": "2026-07-15T16:08:26+08:00",
     "art/blank.md": "2025-02-22T23:28:44+08:00"
   }
 }
