e243d5bc6a55e366c57e30d3a25cc82553dd6524
Q1351998764 <57926506+Q1351998764@users.noreply.github.com>
2026-07-15 08:09:22 +0000

    chore: update text config

--- static/scripts/text-config.js
+++ static/scripts/text-config.js
@@ -24,26 +24,9 @@ export default {
         }
       ],
       "uploadedAt": "2026-07-15T16:08:26+08:00"
-    },
-    {
-      "id": "blank",
-      "title": "Lorem ipsum",
-      "category": "default",
-      "sensitive": false,
-      "documents": [
-        {
-          "path": "art/blank.md",
-          "title": "Lorem ipsum",
-          "excerpt": "Lorem ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sequi itaque blanditiis repellendus aspernatur vitae modi? Eum adipisci unde quia perferendis aspernatu...",
-          "markdown": "# Lorem ipsum\n\nLorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum sequi itaque blanditiis repellendus aspernatur vitae modi?\nEum adipisci unde quia perferendis aspernatur, dicta ullam officiis sapiente illum expedita perspiciatis ex!\n",
-          "uploadedAt": "2025-02-22T23:28:44+08:00"
-        }
-      ],
-      "uploadedAt": "2025-02-22T23:28:44+08:00"
     }
   ],
   "uploads": {
-    "art/20260715-080823-说话也有恐怖谷效应吗-.md": "2026-07-15T16:08:26+08:00",
-    "art/blank.md": "2025-02-22T23:28:44+08:00"
+    "art/20260715-080823-说话也有恐怖谷效应吗-.md": "2026-07-15T16:08:26+08:00"
   }
 }
